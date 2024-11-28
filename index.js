3dc23c6d28915c20c98135b5d23f8189fd0a2f0d
Sushant <sushantghadge2016@gmail.com>
2024-11-28 22:53:32 +0530

    urlShortner api created

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
