3b9f4ccc17097a72d81b2fe3275fd89b2d330cbd
Sushant <sushantghadge2016@gmail.com>
2024-11-28 22:48:43 +0530

    urlShortner api created

--- index.js
+++ index.js
@@ -5,41 +5,28 @@ import cors from 'cors';
 import urlRoutes from './src/routes/urlRoutes.js'; 
 
 dotenv.config();
-
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://sushantghadge2016:Zg8BjIJQF7TdWzU1@cluster0.mk0sf.mongodb.net/';
-
-const ConnectDB = async () => {
-  try {
-    if (!mongoURI) {
-      throw new Error("MongoDB URI is undefined. Please set the MONGODB_URI environment variable.");
-    }
-
-    const connection = await mongoose.connect(mongoURI, {
-    });
-
-    console.log(`MongoDB Connected Successfully: ${connection.connection.host}`);
-  } catch (err) {
-    console.log(`Error: ${err.message}`);
-    process.exit();
-  }
-};
-
-ConnectDB();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is Running Successfully");
-});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
+
+
+app.get("/", (req, res) => {
+    res.send("API is Running Sucessfully");
+})
 app.use('/api', urlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-export default app;
+export default app; 
