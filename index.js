488658d98edf4b9adf82471eca74634f68c10f42
Sushant <sushantghadge2016@gmail.com>
2024-11-28 22:16:25 +0530

    urlShortner api created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import urlRoutes from './src/routes/urlRoutes.js'; 
+const mongoURI = process.env.MONGODB_URI;
 
 dotenv.config();
 const app = express();
@@ -11,13 +12,16 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
-  app.get("/", (req, res) => {
+
+
+
+app.get("/", (req, res) => {
     res.send("API is Running Sucessfully");
 })
 app.use('/api', urlRoutes);
